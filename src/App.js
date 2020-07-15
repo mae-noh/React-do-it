554136a30d8966b3d2a1b78ed13f52616b7799e0
myeongheeroh <myeongheeroh@gmail.com>
2020-07-15 23:45:20 +0900

    useEffect() : clean up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 //import Say from './Say';
 //import EventPractice from './EventPractice';
 //import IterationSample from './IterationSample';
@@ -6,7 +6,16 @@ import React from 'react';
 import Info from './Info';
 
 const App = () => {
-  return <Info />;
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      <button onClick={() => { setVisible(!visible) }}>
+        {visible ? '숨기기' : '보이기'}
+      </button>
+      <hr />
+      {visible && <Info />}
+    </div>
+  );
 };
 
 export default App;
--- src/Info.js
+++ src/Info.js
@@ -5,7 +5,12 @@ const Info = () => {
     const [nickname, setNickname] = useState('');
 
     useEffect(() => {
-        console.log('마운트 될 때만 실행');
+        console.log('effect');
+        console.log(name);
+        return () => {
+            console.log('cleanup');
+            console.log(name);
+        };
     }, []);
 
     const onChangeName = e => {
