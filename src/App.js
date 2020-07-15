d817dbd0b2a4df90ba63f7f547b37f58dce2a626
myeongheeroh <myeongheeroh@gmail.com>
2020-07-16 01:26:12 +0900

    useState() : getAverage

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React, { useState } from 'react';
 //import EventPractice from './EventPractice';
 //import IterationSample from './IterationSample';
 //import Counter from './Counter';
-import Info from './Info';
+//import Info from './Info';
+import Average from './Average';
 
 const App = () => {
   const [visible, setVisible] = useState(false);
@@ -13,7 +14,7 @@ const App = () => {
         {visible ? '숨기기' : '보이기'}
       </button>
       <hr />
-      {visible && <Info />}
+      {visible && <Average />}
     </div>
   );
 };
