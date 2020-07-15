f04838ec6a64a653da412bcb5826b707a2bcead0
myeongheeroh <myeongheeroh@gmail.com>
2020-07-15 22:58:49 +0900

    useState() Info

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 //import Say from './Say';
 //import EventPractice from './EventPractice';
-import IterationSample from './IterationSample';
+//import IterationSample from './IterationSample';
+import Info from './Info';
 
 const App = () => {
-  return <IterationSample />;
+  return <Info />;
 };
 
 export default App;
---
+++ src/Info.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+const Info = () => {
+    const [name, setName] = useState('');
+    const [nickname, setNickname] = useState('');
+
+    const onChangeName = e => {
+        setName(e.target.value);
+    };
+
+    const onChangeNickname = e => {
+        setNickname(e.target.value);
+    };
+
+    return (
+        <div>
+            <input
+                type="text"
+                name="name"
+                value={name}
+                placeholder="이름을 입력하세요."
+                onChange={onChangeName}
+            />
+            <input
+                type="text"
+                name="nickname"
+                value={nickname}
+                placeholder="닉네임을 입력하세요."
+                onChange={onChangeNickname}
+            />
+            <h1>이름:{name} </h1>
+            <h1>닉네임:{nickname}</h1>
+        </div>
+    );
+};
+
+export default Info;
